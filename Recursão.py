394308cad205bd4c3dc482a5a0d0d2d6edb5c2ff
Leonardo Arthur S. Zanardi <84106410+LZanardii@users.noreply.github.com>
2022-08-17 21:39:15 -0300

    Update Recursão.py
    update file

--- Recursão.py
+++ Recursão.py
@@ -1,4 +1,3 @@
-#pesquisa sequencial recursiva
 def pesquisa_sequencial(array, key):
   def pesquisa_sequecial_recursao(array, key, index):
     if len(array) == index:
@@ -8,25 +7,22 @@ def pesquisa_sequencial(array, key):
     return pesquisa_sequecial_recursao(array, key, index + 1)
   return pesquisa_sequecial_recursao(array, key, 0)
 
-#pesquisa binaria recursiva
 def pesquisa_binaria(array, key):
   def pesquisa_binaria_recursiva(array, key, inf, sup):
     #caso base
     if inf > sup:
       return -1
-      
     middle = (inf + sup) // 2 
-    
     if key == array[middle]:
       return middle
     elif key < array[middle]:
       return pesquisa_binaria_recursiva(array, key, inf, middle - 1)
     else:
-      return pesquisa_binaria_recursiva(array, key, middle + 1, sup)pesquisa_binaria_recursiva(array, key, 0, len(array) - 1)
+      return pesquisa_binaria_recursiva(array, key, middle + 1, sup)
+  return pesquisa_binaria_recursiva(array, key, 0, len(array) - 1)
 
 if __name__ == "__main__":
   array = [1,2,3,4,5,6,7,8,9,10]
   print(pesquisa_sequencial(array, 6))
-  print(pesquisa_binaria(array, 9))
-  return 
+  print(pesquisa_binaria(array, 6))
   
